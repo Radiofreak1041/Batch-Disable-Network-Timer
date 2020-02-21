c0e0233651e7fe398a18b101f9f3e4da3bd50a31
Radiofreak1041 <rdsserver@outlook.com>
2020-02-21 18:13:58 +0100

    Initial commit

---
+++ Disable Internet.bat
@@ -0,0 +1,17 @@
+@echo off
+echo Ethernet adapter disabled for 20 seconds in
+echo.
+echo 5
+ping -n 2 127.0.0.1>nul
+echo 4
+ping -n 2 127.0.0.1>nul
+echo 3
+ping -n 2 127.0.0.1>nul
+echo 2
+ping -n 2 127.0.0.1>nul
+echo 1
+ping -n 2 127.0.0.1>nul
+
+netsh interface set interface name="Ethernet" disable
+TIMEOUT 20
+netsh interface set interface name="Ethernet" enable
